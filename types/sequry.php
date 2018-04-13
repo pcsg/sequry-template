a4c1e0c8fb225b1ff2eddf02aa79f71b7722c211
michael.daniel <michael@pcsg.de>
2018-04-13 17:17:13 +0200

    feat: Login Panel.

--- types/sequry.php
+++ types/sequry.php
@@ -1,2 +1,17 @@
 <?php
 
+use QUI\FrontendUsers\Controls\Auth\FrontendLogin;
+
+
+// todo wenn Projektlogo und Placeholder nicht eingestellt sind, soll SEQURY Logo verwendet werden.
+$Logo = $Project->getMedia()->getLogoImage();
+
+$FrontendLogin = new FrontendLogin([
+    'showRegistration' => false
+]);
+
+
+$Engine->assign([
+    'Logo' => $Logo,
+    'FrontendLogin' => $FrontendLogin
+]);
