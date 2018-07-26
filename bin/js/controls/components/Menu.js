db746ca447806c568f31735f632b9daa3e78b7cd
michael.daniel <michael@pcsg.de>
2018-07-26 11:00:17 +0200

    feat: Filter in Nav as controls (with no function currently).

--- bin/js/controls/components/Menu.js
+++ bin/js/controls/components/Menu.js
@@ -19,11 +19,49 @@ define('package/sequry/template/bin/js/controls/components/Menu', [
         Type   : 'package/sequry/template/bin/js/controls/components/Menu',
 
         Binds: [
-            '$onInject'
+            '$onInject',
+            '$buildFilters',
+            'createEntry'
         ],
 
         initialize: function (options) {
             this.parent(options);
+
+            this.FilterContainer = null;
+            this.TypesContainer = null;
+            this.TagsContainer = null;
+
+            // available filters
+            this.$filters = {
+                'all': {
+                    label: 'Alle Passwörter',
+                    icon: 'fa fa-lock'
+                },
+                'favorites': {
+                    label: 'Favoriten',
+                    icon: 'fa fa-star-o'
+                },
+                'owned': {
+                    label: 'Eigene',
+                    icon: 'fa fa-user-o'
+                },
+                'mostUsed': {
+                    label: 'Meistgenutzte',
+                    icon: 'fa fa-bookmark-o'
+                },
+                'new': {
+                    label: 'Neueste',
+                    icon: 'fa fa-clock-o'
+                }
+            };
+
+            this.$types = [
+                'website',
+                'secretkey',
+                'ftp',
+                'apikey',
+                'text'
+            ];
 
             this.addEvents({
                 onInject: this.$onInject
@@ -34,17 +72,50 @@ define('package/sequry/template/bin/js/controls/components/Menu', [
          * event: on inject
          */
         $onInject: function () {
-//            console.log(this.$Elm);
-//            console.log('on inject');
-
             this.$Elm.set('html', Mustache.render(Template, {}));
 
+            this.FilterContainer = this.$Elm.getElement('.sequry-filter .navigation');
+            this.TypesContainer = this.$Elm.getElement('.sequry-passwordType .navigation');
+            this.TagsContainer = this.$Elm.getElement('.sequry-tags .navigation');
+
+            this.$buildFilters();
+
             // favorite
-            this.Favorite = this.$Elm.getElement('#favorite');
+            /*this.Favorite = this.$Elm.getElement('#favorite');
 
             this.Favorite.addEvent('click', function () {
                 window.PasswordList.showFavorite();
+            })*/
+        },
+
+        $buildFilters: function () {
+            var self = this;
+            Object.each(this.$filters, function(key) {
+              self.createEntry(key)
             })
+        },
+
+        createEntry: function (entry) {
+            var icon = '<span class="navigation-entry-icon ' + entry.icon + '"></span>',
+                label = '<span class="navigation-entry-text">' + entry.label + '</span>';
+
+            var listEntry = new Element('li', {
+                'class': 'navigation-entry aaaaaaaa'
+            });
+
+            new Element('a', {
+                html: icon + label,
+                events: {
+                    click: function() {
+                        console.log(entry.label);
+                    }
+                }
+            }).inject(listEntry);
+
+            listEntry.inject(this.FilterContainer);
+
         }
+
+
     });
 });
