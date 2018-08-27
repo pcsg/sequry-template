fa0f2c19d332ec0151a5b024bea7f274ef5cae1c
michael.daniel <michael@pcsg.de>
2018-08-27 08:14:11 +0200

    refactor: Path to AuthDactors Control changed.

---
+++ src/Sequry/Template/Controls/Profile/AuthFactors.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * This file contains QUI\Sequry\Template\Controls\Profile\AuthFactors
+ */
+namespace QUI\Sequry\Template\Controls\Profile;
+
+use QUI;
+use QUI\FrontendUsers\Controls\Profile;
+use QUI\Control;
+use QUI\FrontendUsers\Controls\Profile\ControlInterface;
+
+class AuthFactors extends Control implements ControlInterface
+{
+
+    public function __construct(array $attributes = array())
+    {
+        QUI\System\Log::writeRecursive('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
+        parent::__construct($attributes);
+
+        /*$this->setAttributes(array(
+            'class'    => 'teeeeeeeeeeeeest33333333333',
+
+
+            'quiClass' => 'package/sequry/core/bin/controls/user/AuthPluginSettings'
+
+        ));*/
+
+
+
+//        $this->addCSSClass('teeeeeeeeeeeeest1');
+//        $this->addCSSClass('teeeeeeeeeeeeeeeest2222');
+//        $this->addCSSFile(dirname(__FILE__).'/UserData.css');
+
+    }
+
+    /**
+     * @return string
+     */
+    public function getBody()
+    {
+        $Engine = QUI::getTemplateManager()->getEngine();
+
+        return $Engine->fetch(dirname(__FILE__).'/AuthFactors.html');
+    }
+
+    /**
+     * event: on save
+     */
+    public function onSave()
+    {
+//        $Request = QUI::getRequest();
+//
+//        if (!$Request->request->get('profile-save')) {
+//            return;
+//        }
+//
+//        /* @var $User QUI\Interfaces\Users\User */
+//        $data = $Request->request->all();
+//        $User = $this->getAttribute('User');
+//
+//        $User->setAttributes($data);
+//        $User->save();
+    }
+
+
+    public function validate()
+    {
+
+    }
+}
