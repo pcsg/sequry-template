8ffd0908a25b9bbec1faca6c094cf980e1dcc5c2
michael.daniel <michael@pcsg.de>
2018-08-29 10:21:20 +0200

    refactor: Sequry Logo as default if no logo available.

--- index.php
+++ index.php
@@ -3,8 +3,11 @@
 use QUI\FrontendUsers\Controls\Auth\FrontendLogin;
 use Sequry\Core\Constants\Sequry as SequryConstants;
 
-$Logo = $Project->getMedia()->getLogoImage();
+$logoUrl = URL_OPT_DIR . 'sequry/template/bin/images/Sequry_logo_250x40.png';
 
+if ($Project->getMedia()->getLogoImage()) {
+    $logoUrl = $Project->getMedia()->getLogoImage()->getSizeCacheUrl();
+}
 
 $FrontendLogin = new FrontendLogin([
     'showRegistration' => false
@@ -19,7 +22,7 @@ $Avatar = new QUI\FrontendUsers\Controls\UserIcon([
 ]);
 
 $Engine->assign([
-    'Logo'               => $Logo,
+    'logoUrl'               => $logoUrl,
     'FrontendLogin'      => $FrontendLogin,
     'isAuth'             => $isAuth,
     'SessionUser'        => $SessionUser,
